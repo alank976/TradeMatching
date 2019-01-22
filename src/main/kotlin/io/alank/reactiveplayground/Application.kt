8c9206b1b4567725c2b1780e0bf39ccc90d959bf
alank <alankan976@gmail.com>
2019-01-22 22:01:46 +0800

    Enable Mongo audit for createdTime and configure local mongodb offered by docker hub

--- src/main/kotlin/io/alank/reactiveplayground/Application.kt
+++ src/main/kotlin/io/alank/reactiveplayground/Application.kt
@@ -2,7 +2,9 @@ package io.alank.reactiveplayground
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 
+@EnableMongoAuditing
 @SpringBootApplication
 class ReactivePlaygroundApplication
 
--- src/main/kotlin/io/alank/reactiveplayground/domain/trade/Trade.kt
+++ src/main/kotlin/io/alank/reactiveplayground/domain/trade/Trade.kt
@@ -1,13 +1,16 @@
 package io.alank.reactiveplayground.domain.trade
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import java.time.LocalDateTime
 
 data class Trade(@Id val id: String? = null,
                  val ticker: String,
                  val way: Way,
                  val price: Double,
                  val quantity: Long,
-                 val account: String? = null) {
+                 val account: String? = null,
+                 @CreatedDate val insertionDate: LocalDateTime? = null) {
     override fun equals(other: Any?) = id == (other as Trade).id
 }
 
