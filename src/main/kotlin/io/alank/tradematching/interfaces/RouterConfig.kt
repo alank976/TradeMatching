824d81a064757280fddec62955ef92c811a482e4
alank <alankan976@gmail.com>
2019-03-19 11:39:48 +0800

    Expose MatchingService

--- src/main/kotlin/io/alank/tradematching/interfaces/RouterConfig.kt
+++ src/main/kotlin/io/alank/tradematching/interfaces/RouterConfig.kt
@@ -5,24 +5,27 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
-import java.net.URI
 
 @Configuration
 class RouterConfig {
     @Bean
-    fun apiRouter(handler: TradeHandler) = router {
+    fun apiRouter(tradeHandler: TradeHandler,
+                  matchingHandler: MatchingHandler) = router {
         GET("/") {
             ServerResponse.ok().build()
         }
         "/api".nest {
             (accept(APPLICATION_JSON) and "/v1").nest {
                 "/trades".nest {
-                    GET("/", handler::getAll)
-                    POST("/", handler::save)
+                    GET("/", tradeHandler::getAll)
+                    POST("/", tradeHandler::save)
 //                        "/{id}".nest {
-//                            GET("/", handler::getOne)
-//                            DELETE("/", handler::delete)
+//                            GET("/", tradeHandler::getOne)
+//                            DELETE("/", tradeHandler::delete)
 //                        }
+                }
+                "/matching".nest {
+                    GET("/", matchingHandler::match)
                 }
             }
         }
